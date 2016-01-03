3c9bec4393288e9e7d12abdc2672c2d0b8754ae1
simon <mahuan1768@126.com>
2016-01-03 07:08:35 -0800

    添加测试私有方法的方法  Changes to be committed: 	modified:   tests/framework/thinkphp/library/think/ViewTest.php

--- tests/framework/thinkphp/library/think/ViewTest.php
+++ tests/framework/thinkphp/library/think/ViewTest.php
@@ -29,12 +29,119 @@ class viewTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * 句柄测试
+     * 测试变量赋值
      * @return  mixed
      * @access public
      */
     public function testAssign()
     {
+        $view_instance = \think\View::getInstance();
+        $data = $view_instance->assign(array('key'=>'value'));
+        $data = $view_instance->assign('key', 'value');
+        //测试私有属性
+        $this->assertAttributeEquals('value','pr',$view_instance); 
+        //测试私有方法
+        $method = new \ReflectionMethod('\think\View','test');
+        $method->setAccessible(TRUE); 
+        $this->assertEquals('test',$method->invoke($view_instance));
+
     }
-    
+
+    /**
+     *  测试配置
+     * @return  mixed
+     * @access public
+     */
+    public function testConfig()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->config('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    } 
+
+
+    /**
+     *  测试引擎设置
+     * @return  mixed
+     * @access public
+     */
+    public function testEngine()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->engine('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    }  
+
+    /**
+     *  测试引擎设置
+     * @return  mixed
+     * @access public
+     */
+    public function testTheme()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->engine('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    } 
+
+
+    /**
+     *  测试引擎设置
+     * @return  mixed
+     * @access public
+     */
+    public function testFetch()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->engine('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    }   
+
+    /**
+     *  测试显示
+     * @return  mixed
+     * @access public
+     */
+    public function testShow()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->engine('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    } 
+
+    /**
+     *  测试引擎设置
+     * @return  mixed
+     * @access public
+     */
+    public function testParseTemplate()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->engine('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    }
+
+    /**
+     *  测试引擎设置
+     * @return  mixed
+     * @access public
+     */
+    public function testGetTemplateTheme()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->engine('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    }
+
+    /**
+     *  测试引擎设置
+     * @return  mixed
+     * @access public
+     */
+    public function testGetThemePath()
+    {
+        $view_instance = \think\View::getInstance();
+        $data = $view->engine('key', 'value');
+        $data = $view->engine('engine', array('key'=>'value'));
+    }
 }
